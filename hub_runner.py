e3242ae5aabc810d85013982f3587807d93ada44
yana sereda <sereda.yana90@yandex.ru>
2022-05-25 16:12:55 +0300

    in pro

--- hub_runner.py
+++ hub_runner.py
@@ -3,7 +3,7 @@ from hub import *
 class HubRunner:
     def __init__(self):
         self.i_hub_runner = IHubRunner()
-        self.or_hub_runner = OrHubRunner()
+        self.or_rw_hub_runner = OrRwHubRunner()
         self.and_hub_runner = AndHubRunner()
         self.root_hub_runner = RootHubRunner()
 
@@ -12,8 +12,8 @@ class HubRunner:
             return self.i_hub_runner.run(hub, context)
         if type(hub)==AndHub:
             return self.and_hub_runner.run(hub, context)
-        if type(hub)==OrHub:
-            return self.or_hub_runner.run(hub, context)
+        if type(hub)==OrRwHub:
+            return self.or_rw_hub_runner.run(hub, context)
         if type(hub) == RootHub:
             return self.root_hub_runner.run(hub, context)
         assert False, "prop err: unknown type of hub"
@@ -66,15 +66,72 @@ class AndHubRunner:
         #  В узел пришли экземпляры (положились в  self.input_exs_obj).
         #  Они могли придти справа или слева.
         if hub.input_exs_obj.sender.SUPER_ID == hub.child_left_SUPER_ID:
-            next_hub = hub._propagate_exemplars_from_left(context)
+            next_hub = hub._propagate_exemplars_from_left(hub, context)
             return next_hub
         assert hub.input_exs_obj.sender.SUPER_ID == self.child_right.SUPER_ID, "prop err: super_id of child wrong"
-        next_hub = hub._propagate_exemplars_from_right(context)
+        next_hub = hub._propagate_exemplars_from_right(hub, context)
         return next_hub
 
+    def _propagate_exemplars_from_right(self, hub, context):
+        #  1) продолжается старый росток (т.е.в И-узел снизу зашел тот же росток,
+        #  что когда-то его создал, идя сверху)
+        if hub.input_exs_obj.sender.ID == hub.child_right.ID:
+            hub.child_right_exemplars = deepcopy(hub.input_exs_obj.exemplars)
+            if hub.child_left_exemplars is not None:  # все есть для попытки запуска себя!
+                assert hub.main_conditioning_child_is_left is True, "prop err: main child must be left"
+                exemplars = hub.signa.run(left_pre_exemplars=hub.child_left_exemplars,
+                                           right_pre_exemplars=hub.child_right_exemplars)
+                if exemplars is None:
+                    return None  # провал на узле (росток умер)
+                hub.parent.set_input_exemplars(exemplars, sender=hub)
+                return hub.parent
+            else: # запуск себя невозможен ввиду отсуствия экземпляров слева
+                # чтоб их (когда-нибудь) получить, создаем левого ребенка и передаем ему управление
+                assert hub.main_conditioning_child_is_left is False, "prop err: main child must be right"
+                hub.child_left = self._create_left_child_by_right_exemplars(hub, context)
+                del hub.input_exs_obj
+                hub.input_exs_obj = None
+                return hub.child_left
+        else:
+        # 2 ) в узел зашел какой-то новый росток, и нужно продоложить
+        # его участок по каркасу этого И-узла
+            # пересоздаем И у-зел----------------------------------------------
+            new_and_hub = deepcopy(hub)
+            new_and_hub.ID = context.get_id()
+            # настраиваем правого ребенка--------------------------------------
+            new_and_hub.child_right = hub.input_exs_obj.sender
+            # настраиваем левого ребенка--------------------------------------
+            # если  новый росток зашел из главного ребенка, то
+            # ведомый ребенок должен быть в этом ростке перечсчитан:
+            if hub.main_conditioning_child_is_left is False:
+                new_and_hub.child_left = None
+                new_and_hub.child_left_exemplars = None
+            return new_and_hub
+
+    def _create_left_child_by_right_exemplars(self, hub, context):
+        right_points = extract_cloud_from_exemplars_list_by_eid(hub.signa.pre_eid_right, hub.child_right_exemplars)
+        left_points = hub.signa.get_left_cloud_by_right_cloud(right_points)
+        new_left_eid = hub.signa.get_new_eid_left()
+        condition = Condition(new_left_eid, list(left_points))
+        child = context.create_hub(parent=hub,
+                                   SUPER_ID=hub.child_left_SUPER_ID,
+                                   condition=condition)
+        return child
+
+    def _create_right_child_by_left_exemplars(self, hub, context):
+        left_points = extract_cloud_from_exemplars_list_by_eid(hub.signa.pre_eid_left, hub.child_left_exemplars)
+        right_points = hub.signa.get_right_cloud_by_left_cloud(left_points)
+        new_right_eid = hub.signa.get_new_eid_right()
+        condition = Condition(new_right_eid, list(right_points))
+        child = context.create_hub(parent=hub,
+                                   SUPER_ID=self.child_right_SUPER_ID,
+                                   condition=condition)
+        return child
 
 
-class OrHubRunner:
+
+
+class OrRwHubRunner:
     def run(self, hub, context):
         pass
 
