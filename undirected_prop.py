54835a84db3caa588582387145427289cef2d837
yana sereda <sereda.yana90@yandex.ru>
2022-05-27 19:06:00 +0300

    architecture for undirected_prop is done!

--- undirected_prop.py
+++ undirected_prop.py
@@ -11,6 +11,29 @@
 # try starter_id = pre_eid2 ...
 # .... test...
 
+from prog_exemplar import ProgExemplar
+from prop import get_exemplars_by_condition
+
+class Possibility:
+    def __init__(self, point_cloud_of_eid, eid, prognames_to_start):
+        self.point_cloud_of_eid=point_cloud_of_eid
+        self.eid=eid
+        self.prognames_to_start=prognames_to_start
+
+    def is_fully_done(self):
+        if len(self.prognames_to_start)==0:
+            return True
+        return False
+
+def _exemplars_to_possibilities(exemplars):
+    return possibilities
+
+def _try_run_possibility(possibility):
+    return exemplars
+
+def _init_possibilities(point):
+    return possibilities
+
 
 def undirected_prop_from_point(point, context):
     # запускаем для этой точки все IProgs
@@ -31,7 +54,24 @@ def undirected_prop_from_point(point, context):
 
     # Если очережная программа не срабатывает, то переключаемся на следующую возможность.
     # Если возможностей больше нет, алгоритм завершен!
+    possibilities = _init_possibilities(point)
+    if possibilities is None:
+        return
+
+    while True:
+        if len(possibilities)==0:
+            break
+        possibility = possibilities[0]
+        exemplars = _try_run_possibility(possibility)
+        if exemplars is None:
+            possibilities.pop()
+        else:
+            new_possibilities = _exemplars_to_possibilities(exemplars)
+            if new_possibilities is not None:
+                possibilities = possibilities + new_possibilities
+                if possibility.is_fully_done():
+                    possibilities.pop()
 
 
 
-    pass
+
